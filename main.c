b4274b396160a5370304f1f7b5141a7f254ed827
Chokunnotlikeus <ittikorn.chokun@gmail.com>
2025-08-04 18:33:51 +0800

    Odd or Even project
    Project Description:
    This project serves as a foundation for learning and improving my basic C programming skills. The main functionality of the project is to implement a program that separates numbers into even and odd categories. It demonstrates the use of conditional statements, loops, and basic input/output operations in C.

---
+++ main.c
@@ -0,0 +1,20 @@
+//---Odd or Even Project ---//
+#include <stdio.h> //นำเข้าคำสั่งทั้งหมด
+int main() 
+{
+  //data
+  int number ;
+  
+  // input
+printf("Input number : ") ;
+scanf("%d", &number) ;
+if(number%2 ==0){
+printf("Even Number !!!0 \n ") ;
+
+}else {
+printf("Odd Number !!! \n") ;
+
+}
+   return 0 ;
+
+ }
