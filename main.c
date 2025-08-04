0360c1bc8b1ba0491deaaedd75f2f06cede12647
Chokunnotlikeus <ittikorn.chokun@gmail.com>
2025-08-04 18:48:42 +0800

    Even or Odd project

---
+++ main.c
@@ -0,0 +1,20 @@
+//---Odd or Even Project ---//
+#include <stdio.h> //
+int main() 
+{
+  //data
+  int number ;
+  
+  // input
+printf("Input number : ") ;
+scanf("%d", &number) ;
+if(number%2 ==0){
+printf("Even Number !!! \n ") ;
+
+}else {
+printf("Odd Number !!! \n") ;
+
+}
+   return 0 ;
+
+ }
